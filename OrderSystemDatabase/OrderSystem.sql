6bb6977d10ca971410d6ff65e6a99f74ebd46e1a
Eli-Mao <1426635781@qq.com>
2024-05-23 18:19:08 +0800

    order表设置外键（U_PersonID）

--- OrderSystemDatabase/OrderSystem.sql
+++ OrderSystemDatabase/OrderSystem.sql
@@ -1,108 +1,119 @@
-create database orderSystem;
-USE orderSystem;
-    create table `order`
-(
-    OrderID       CHAR(8)     not null
-        primary key,
-    Date          DATETIME       not null,
-    PaymentStatus ENUM ('PAID', 'UNPAID') not null,
-    PersonID    CHAR(6)     not null,
-    UNIQUE (OrderID),
-    FOREIGN KEY (PersonID) REFERENCES person(PersonID) ON DELETE CASCADE
-);
-
 create table orderlist
 (
-    OrderID  CHAR(8) not null
+    OrderID  int(8) not null
         primary key,
-    DishID   CHAR(4) not null,
-    Comments VARCHAR(100)   null,
-    FOREIGN KEY (OrderID) REFERENCES `order`(OrderID) ON DELETE CASCADE,
-    FOREIGN KEY (DishID) REFERENCES dish(DishID) ON DELETE CASCADE
+    DishID   int(4) not null,
+    Comments char   null,
+    constraint DishID
+        unique (DishID)
 );
 
 create table person
 (
-    PersonID    CHAR(6)                  not null
+    PersonID    int(6)                  not null
         primary key,
     Fname       char                    not null,
     Lname       char                    not null,
     password    int                     not null,
     PhoneNumber int                     not null,
-    Gender      enum ('male', 'female') not null,
-    UNIQUE (PersonID)
+    Gender      enum ('male', 'female') not null
+);
+
+create table deliveryperson
+(
+    PersonID     int  not null
+        primary key,
+    DeliveryArea char not null,
+    Status       char not null,
+    constraint DeliveryPerson_person_PersonID_fk
+        foreign key (PersonID) references person (PersonID)
+);
+
+create table `order`
+(
+    OrderID       int(8)     not null
+        primary key,
+    Date          date       not null,
+    PaymentStatus tinyint(1) not null,
+    U_PersonID    int(6)     not null,
+    constraint order_ibfk_1
+        foreign key (U_PersonID) references person (PersonID)
 );
 
 create table deliver
 (
-    OrderID               char(8)                                       not null
+    OrderID               int                                       not null
         primary key,
-    Deli_PersonID         char(6)                                       not null,
-    DeliveryStatus        enum ('DELIVERING', 'WAITING_FOR_DELIVERY', 'ARRIVED') not null,
-    foreign key (OrderID) references `order` (OrderID) ON DELETE CASCADE,
-    foreign key (Deli_PersonID) references person (PersonID)
+    Deli_PersonID         int                                       not null,
+    DeliveryStatus        enum ('delivering', 'waiting', 'resting') not null,
+    EstimatesDeliveryTime datetime                                  not null,
+    constraint Deliver_order_OrderID_fk
+        foreign key (OrderID) references `order` (OrderID),
+    constraint Deliver_person_PersonID_fk
+        foreign key (Deli_PersonID) references person (PersonID)
 );
-create table deliveryPerson
-(
-    PersonID     CHAR(8)  not null
-        primary key,
-    DeliveryArea char not null,
-    Status       ENUM ('DELIVERING', 'WAITING', 'RESTING') not null,
-    foreign key (PersonID) references person (PersonID) ON DELETE CASCADE
-);
+
+create index U_PersonID
+    on `order` (U_PersonID);
 
 create table restaurant
 (
-    RestaurantID       CHAR(7) not null
+    RestaurantID       int(7) not null
         primary key,
     RestaurantName     char   not null,
     Address            char   not null,
     ContactInformation char   not null,
     BusinessHours      char   not null,
-    M_PersonID         int(6) not null,
-    UNIQUE (RestaurantID)
+    M_PersonID         int(6) not null
 );
 
 create table dish
 (
-    DishID         CHAR(4)     not null
+    DishID         int(4)     not null
         primary key,
     Name           char       not null,
     Price          float      not null,
-    Availability   ENUM('AVAILABLE', 'UNAVAILABLE') NOT null,
+    Availability   tinyint(1) null,
     Category       char       not null,
-    D_RestaurantID CHAR(7)     not null,
-    foreign key (D_RestaurantID) references restaurant (RestaurantID)
+    D_RestaurantID int(7)     not null,
+    constraint D_RestaurantID
+        foreign key (D_RestaurantID) references restaurant (RestaurantID)
 );
 
-create table restaurantManager
+create table restaurantmanager
 (
-    PersonID           CHAR(6) not null
+    PersonID           int(6) not null
         primary key,
-    DateOfStartManager date   not null,
-    RestaurantID       CHAR(7) not null,
-    foreign key (PersonID) references person (PersonID) ON DELETE CASCADE,
-    foreign key (RestaurantID) references restaurant (RestaurantID) ON DELETE CASCADE
+    DateofStartManager date   not null,
+    RestaurantID       int(7) not null,
+    constraint restaurantmanager_person_PersonID_fk
+        foreign key (PersonID) references person (PersonID),
+    constraint restaurantmanager_restaurant_RestaurantID_fk
+        foreign key (RestaurantID) references restaurant (RestaurantID)
 );
 
 create table review
 (
-    R_PersonID    CHAR(6) not null,
-    Date          DATETIME   not null,
+    R_PersonID    int(6) not null
+        primary key,
+    Date          date   not null,
     Rating        float  not null,
     ReviewContent char   null,
-    RestaurantID  CHAR(7) not null,
-    PRIMARY KEY (R_PersonID, Date),
-    foreign key (R_PersonID) references person(PersonID) ON DELETE CASCADE,
-    foreign key (RestaurantID) references restaurant (RestaurantID) ON DELETE CASCADE
+    RestaurantID  int(7) not null,
+    constraint Date
+        unique (Date),
+    constraint review_restaurant_RestaurantID_fk
+        foreign key (RestaurantID) references restaurant (RestaurantID)
 );
 
 create table user
 (
-    PersonID CHAR(6) not null
+    PersonID int not null
         primary key,
-    Address CHAR null,
-    phongNumber int(11) not null,
-    foreign key (PersonID) references person (PersonID) ON DELETE CASCADE
+    Address  int null,
+    constraint User_person_PersonID_fk
+        foreign key (PersonID) references person (PersonID)
 );
+
+
 
