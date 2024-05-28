8836a7b43d11c7a7fc5c2dac20a1ab8c0f4043a4
laijiewo <jingwang.li@ucdconnect.ie>
2024-05-28 19:23:11 +0800

    Update RestaurantManager.java

--- orderSystem/src/main/java/module/RestaurantManager.java
+++ orderSystem/src/main/java/module/RestaurantManager.java
@@ -5,16 +5,16 @@ import module.enums.Gender;
 import module.enums.personEnum;
 
 public class RestaurantManager extends Person {
-    private int RestaurantID;
+    private String RestaurantID;
     private final Date startManagementDate;
-    public RestaurantManager(String PersonID, String LastName, String FirsName, String PhoneNumber, String password, Gender Gender, int RestaurantID, Date startManagementDate) {
+    public RestaurantManager(String PersonID, String LastName, String FirsName, String PhoneNumber, String password, Gender Gender, String RestaurantID, Date startManagementDate) {
         super(PersonID, LastName, FirsName, PhoneNumber, password, Gender);
         this.RestaurantID = RestaurantID;
         this.startManagementDate = startManagementDate;
         personType = personEnum.RESTAURANT_MANAGER;
     }
 
-    public int getRestaurantID() {
+    public String getRestaurantID() {
         return RestaurantID;
     }
 
