0ddd1db5e3da395c2490ee9a10c009d1c18b3be8
Eli-Mao <1426635781@qq.com>
2024-05-31 21:44:59 +0800

    Update OrderSystem(1).sql

--- OrderSystemDatabase/OrderSystem(1).sql
+++ OrderSystemDatabase/OrderSystem(1).sql
@@ -1,111 +1,150 @@
-create database orderSystem;
-USE orderSystem;
-
-create table person
+create database ordersystem;
+use ordersystem;
+create table if not exists person
 (
-    PersonID    CHAR(6)                  not null
+    PersonID    char(6)                 not null
         primary key,
-    Fname       VARCHAR(50)                    not null,
-    Lname       VARCHAR(50)              not null,
-    password    VARCHAR(25)              not null,
-    PhoneNumber     CHAR(11)            not null,
+    Fname       varchar(50)             not null,
+    Lname       varchar(50)             not null,
+    password    varchar(25)             not null,
+    PhoneNumber char(11)                not null,
     Gender      enum ('male', 'female') not null,
-    UNIQUE (PersonID)
+    constraint PersonID
+        unique (PersonID)
 );
 
-create table restaurant
+create table if not exists deliveryperson
 (
-    RestaurantID       CHAR(7) not null
+    PersonID     char(8)                                                                                                                               not null
         primary key,
-    RestaurantName     VARCHAR(20)   not null,
-    Address            VARCHAR(50)   not null,
-    ContactInformation VARCHAR(50)   not null,
-    BusinessHours      VARCHAR(50)   not null,
-    M_PersonID         CHAR(6)       not null,
-    UNIQUE (RestaurantID)
-);
-    create table `order`
-(
-    OrderID       CHAR(8)        not null
-        primary key,
-    Date          DATETIME       not null,
-    PaymentStatus boolean        not null,
-    PersonID      CHAR(6)        not null,
-    UNIQUE (OrderID),
-    FOREIGN KEY (PersonID) REFERENCES person(PersonID) ON DELETE CASCADE ON UPDATE CASCADE
+    DeliveryArea enum ('FIRST_TEACHING_BUILDING', 'SECOND_TEACHING_BUILDING', 'THIRD_TEACHING_BUILDING', 'FOURTH_TEACHING_BUILDING', 'GYM', 'LIBRARY') not null,
+    Status       enum ('DELIVERING', 'WAITING', 'RESTING')                                                                                             not null,
+    constraint deliveryperson_ibfk_1
+        foreign key (PersonID) references person (PersonID)
+            on update cascade on delete cascade
 );
 
-create table dish
+create table if not exists `order`
 (
-    DishID         CHAR(4)     not null
+    OrderID  char(8)  not null
         primary key,
-    Name           VARCHAR(50)        not null,
+    Date     datetime not null,
+    PersonID char(6)  not null,
+    constraint OrderID
+        unique (OrderID),
+    constraint order_ibfk_1
+        foreign key (PersonID) references person (PersonID)
+            on update cascade on delete cascade
+);
+
+create table if not exists deliver
+(
+    OrderID        char(8)                                                  not null
+        primary key,
+    Deli_PersonID  char(6)                                                  not null,
+    DeliveryStatus enum ('WAITING_FOR_DELIVERING', 'DELIVERING', 'ARRIVED') not null,
+    constraint deliver_ibfk_1
+        foreign key (OrderID) references `order` (OrderID)
+            on update cascade on delete cascade,
+    constraint deliver_ibfk_2
+        foreign key (Deli_PersonID) references person (PersonID)
+            on update cascade on delete cascade
+);
+
+create index Deli_PersonID
+    on deliver (Deli_PersonID);
+
+create index PersonID
+    on `order` (PersonID);
+
+create table if not exists restaurant
+(
+    RestaurantID       char(7)     not null
+        primary key,
+    RestaurantName     varchar(20) not null,
+    Address            varchar(50) not null,
+    ContactInformation varchar(50) not null,
+    BusinessHours      varchar(50) not null,
+    M_PersonID         char(6)     not null,
+    constraint RestaurantID
+        unique (RestaurantID)
+);
+
+create table if not exists dish
+(
+    DishID         char(4)     not null
+        primary key,
+    Name           varchar(50) not null,
     Price          float       not null,
-    Availability   boolean     not null,
-    D_RestaurantID CHAR(7)     not null,
-    foreign key (D_RestaurantID) references restaurant (RestaurantID)
+    Availability   tinyint(1)  not null,
+    D_RestaurantID char(7)     not null,
+    constraint dish_ibfk_1
+        foreign key (D_RestaurantID) references restaurant (RestaurantID)
+            on update cascade on delete cascade
 );
 
+create index D_RestaurantID
+    on dish (D_RestaurantID);
 
-create table orderlist
+create table if not exists orderlist
 (
-    OrderID  CHAR(8) not null
+    DishID  char(4)      not null,
+    OrderID char(8)      not null,
+    Comment varchar(100) null,
+    number  int          not null,
+    primary key (OrderID, DishID),
+    constraint orderlist_dish_DishID_fk
+        foreign key (DishID) references dish (DishID)
+            on update cascade on delete cascade,
+    constraint orderlist_order_OrderID_fk
+        foreign key (OrderID) references `order` (OrderID)
+            on update cascade on delete cascade
+);
+
+create table if not exists restaurantmanager
+(
+    PersonID           char(6) not null
         primary key,
-    DishID   CHAR(4) not null,
-    Comments VARCHAR(100)   null,
-    FOREIGN KEY (OrderID) REFERENCES `order`(OrderID) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (DishID) REFERENCES dish(DishID) ON DELETE CASCADE ON UPDATE CASCADE
+    DateOfStartManager date    not null,
+    RestaurantID       char(7) not null,
+    constraint restaurantmanager_ibfk_1
+        foreign key (PersonID) references person (PersonID)
+            on update cascade on delete cascade,
+    constraint restaurantmanager_ibfk_2
+        foreign key (RestaurantID) references restaurant (RestaurantID)
+            on update cascade on delete cascade
 );
 
-create table deliver
+create index RestaurantID
+    on restaurantmanager (RestaurantID);
+
+create table if not exists review
 (
-    OrderID               char(8)                                       not null
+    R_PersonID    char(6)      not null,
+    Date          datetime     not null,
+    Rating        float        not null,
+    ReviewContent varchar(100) null,
+    RestaurantID  char(7)      not null,
+    primary key (R_PersonID, Date),
+    constraint review_ibfk_1
+        foreign key (R_PersonID) references person (PersonID)
+            on update cascade on delete cascade,
+    constraint review_ibfk_2
+        foreign key (RestaurantID) references restaurant (RestaurantID)
+            on update cascade on delete cascade
+);
+
+create index RestaurantID
+    on review (RestaurantID);
+
+create table if not exists user
+(
+    PersonID char(6)                                                                                                                               not null
         primary key,
-    Deli_PersonID         char(6)                                       not null,
-    DeliveryStatus        enum ('DELIVERING', 'WAITING_FOR_DELIVERY', 'ARRIVED') not null,
-    foreign key (OrderID) references `order` (OrderID) ON DELETE CASCADE ON UPDATE CASCADE,
-    foreign key (Deli_PersonID) references person (PersonID)
-);
-create table deliveryPerson
-(
-    PersonID     CHAR(8)  not null
-        primary key,
-    DeliveryArea ENUM ('FIRST_TEACHING_BUILDING', 'SECOND_TEACHING_BUILDING', 'THIRD_TEACHING_BUILDING', 'Fourth_TEACHING_BUILDING',
-    'GYM', 'LIBRARY') not null,
-    Status       ENUM ('DELIVERING', 'WAITING', 'RESTING') not null,
-    foreign key (PersonID) references person (PersonID) ON DELETE CASCADE ON UPDATE CASCADE
+    Address  enum ('FIRST_TEACHING_BUILDING', 'SECOND_TEACHING_BUILDING', 'THIRD_TEACHING_BUILDING', 'FOURTH_TEACHING_BUILDING', 'GYM', 'LIBRARY') null,
+    constraint user_ibfk_1
+        foreign key (PersonID) references person (PersonID)
+            on update cascade on delete cascade
 );
 
-
-
-
-create table restaurantManager
-(
-    PersonID           CHAR(6) not null
-        primary key,
-    DateOfStartManager date   not null,
-    RestaurantID       CHAR(7) not null,
-    foreign key (PersonID) references person (PersonID) ON DELETE CASCADE ON UPDATE CASCADE,
-    foreign key (RestaurantID) references restaurant (RestaurantID) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-create table review
-(
-    R_PersonID    CHAR(6) not null,
-    Date          DATETIME   not null,
-    Rating        float  not null,
-    ReviewContent VARCHAR(100)   null,
-    RestaurantID  CHAR(7) not null,
-    PRIMARY KEY (R_PersonID, Date),
-    foreign key (R_PersonID) references person(PersonID) ON DELETE CASCADE ON UPDATE CASCADE,
-    foreign key (RestaurantID) references restaurant (RestaurantID) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-create table user
-(
-    PersonID CHAR(6) not null
-        primary key,
-    Address VARCHAR(50) null,
-    foreign key (PersonID) references person (PersonID) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
+    q`w1`
